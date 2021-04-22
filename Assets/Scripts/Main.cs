e3104bb24677695b673ee385cb9077623a38ff26
Sungki <youbyme@gmail.com>
2021-04-22 18:08:21 -0400

    Created Gird for Nodes
    Created Gird for Nodes.
    Set the positions for Grid.

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -10,12 +10,29 @@ public class Main : MonoBehaviour
     private GameObject nodePrefab;
     private List<GameObject> listNodes = new List<GameObject>();
 
+    private Dictionary<int, Vector3> grid = new Dictionary<int, Vector3>();
+
+    void SetGrid()
+    {
+        int x = 0;
+        int y = 0;
+        for(int i=1; i <= TotalNodes; i++)
+        {
+            grid.Add(i, new Vector3(x++ * 5, y * 5, Height));
+            if(i%10 == 0)
+            {
+                x = 0;
+                y++;
+            }
+        }
+    }
+
     void Start()
     {
+        SetGrid();
         nodePrefab = Resources.Load("Node") as GameObject;
-
-        for (int i = 0; i < TotalNodes; i++)
-            listNodes.Add(Instantiate(nodePrefab, new Vector3(Random.Range(0, 10), Random.Range(0, 10), Height), Quaternion.identity));
+        for (int i = 1; i <= TotalNodes; i++)
+            listNodes.Add(Instantiate(nodePrefab, grid[i], Quaternion.identity));
     }
 
     void Update()
