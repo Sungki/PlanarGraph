f55894a146299ba6da322dfebb54927b45b946dc
Sungki <youbyme@gmail.com>
2021-04-22 17:54:50 -0400

    Created List of nodes
    Created List for nodes

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    [SerializeField] [Range(1,90)] private int TotalNodes = 20;
+    [SerializeField] private float Height = 0;
+
     private GameObject nodePrefab;
+    private List<GameObject> listNodes = new List<GameObject>();
 
     void Start()
     {
         nodePrefab = Resources.Load("Node") as GameObject;
-        Instantiate(nodePrefab);
+
+        for (int i = 0; i < TotalNodes; i++)
+            listNodes.Add(Instantiate(nodePrefab, new Vector3(Random.Range(0, 10), Random.Range(0, 10), Height), Quaternion.identity));
     }
 
     void Update()
